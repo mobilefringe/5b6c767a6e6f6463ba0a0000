3e162e2ce58774831c33aa849cef737a1cd674c8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-04 14:37:16 -0400

    Rancho San Diego 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -254,7 +254,8 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://cerritos.mallmaverick.com/api/v3/cerritos/social.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events")]);
+                        // this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://cerritos.mallmaverick.com/api/v3/cerritos/social.json"})
                         return results;
                     } catch(e) {
                         console.log("Error loading data: " + e.message);    
