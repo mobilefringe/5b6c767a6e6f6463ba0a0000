dbbf55244bac771a079b53afe6ef57d58cabb7ab
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-13 11:13:31 -0400

    Rancho San Diego 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -56,7 +56,8 @@
             },
             computed: {
                 ...Vuex.mapGetters([
-                    'property'
+                    'property',
+                    'findRepoByName'
                 ])
             },
             methods: {
