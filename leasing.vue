e2b42a866c1476112ee30eebdd14de28bde6d583
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-16 11:11:37 -0400

    Rancho San Diego 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -71,9 +71,9 @@
                     
                     var temp_repo1 = this.findRepoByName('Leasing Booklet');
                     console.log(temp_repo1)
-                    if(!_.isEmpty(temp_repo1)) {
-                        this.leasingBooklet = temp_repo1.images[0].image_url;
-                    }
+                    // if(!_.isEmpty(temp_repo1)) {
+                    //     this.leasingBooklet = temp_repo1.images[0].image_url;
+                    // }
 
                     var temp_repo2 = this.findRepoByName('Leasing Images');
                     if(temp_repo2) {
